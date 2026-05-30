ef1ec58155beffcb87d47ef9ccf04745b2257653
Przemek <100935707+PrzemekSwiecinski@users.noreply.github.com>
2026-05-31 00:54:16 +0200

    Update wyloguj.php

--- api/wyloguj.php
+++ api/wyloguj.php
@@ -1,11 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Lub inny adres Reacta
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=utf-8");
-session_start(); // Start the session
+session_start();
 
-// Zniszcz sesję
 session_unset();
 session_destroy();
 
